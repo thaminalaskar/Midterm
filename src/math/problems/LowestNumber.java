256bee87c249eef46ca4408c2794d3dc08c68f5c
Thamina Laskar <bombae@Thaminas-MacBook-Pro.local>
2021-05-04 15:02:16 -0400

    initial commit

--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -15,30 +15,3 @@ public class LowestNumber {
          */
         int  array[] = new int[]{211,110,99,34,67,89,67,456,321,456,78,90,45,32,56,78,90,54,32,123,67,5,679,54,32,65};
 
-        //find lowest number from the array
-        int lowest =array[0];
-
-        for(int i=1;i<array.length;i++) {
-            if (array[i] < lowest) {
-                lowest = array[i];
-            }
-        }
-        System.out.println(lowest);
-
-        ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
-        List<String> lowestValue = new ArrayList<String>();
-        try {
-            connectToSqlDB.insertDataFromArrayToSqlTable(array, "tbl_lowestNumber", "column_lowestNumber");
-            lowestValue = connectToSqlDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println("Data is reading from the Table (tbl_primenumber) and displaying to the console");
-        for(String st:lowestValue){
-            System.out.println(st);
-        }
-
-    }
-
-}
