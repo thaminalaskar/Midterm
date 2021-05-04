3d1786297a776ae0deafda55aac8a2f1aa9fa53e
Thamina Laskar <bombae@Thaminas-MacBook-Pro.local>
2021-05-04 15:51:55 -0400

    initial commit

--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -4,25 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DetermineLargestWord {
-
-    public static void main(String[] args) {
-        /*
-         Implement to Find the length and longest word in the given sentence below.
-         Should return "10 biological".
-         */
-        String s = "Human brain is a biological learning machine";
-        Map<Integer, String> wordNLength = findTheLargestWord(s);
-        //implement
-
-        System.out.println(s);
-
-    }
-
     public static Map<Integer, String> findTheLargestWord(String wordGiven) {
         Map<Integer, String> map = new HashMap<Integer, String>();
-        String st = "";
-        //implement
-
+        String words[] = wordGiven.split(" ");
+        String maxWord = words[0];
+        for (int i = 1; i < words.length; i++) {
+            if (words[i].length() > maxWord.length()) {
+                maxWord = words[i];
+            }
+        }
+        map.put(maxWord.length(), maxWord);
         return map;
     }
+
+    public static void main(String[] args) {
+        String s = "Human brain is a biological learning machine";
+        Map<Integer, String> wordNLength = findTheLargestWord(s);
+        for (String word : wordNLength.values()) {
+            s = word.length() + " " + word;
+        }
+        System.out.println(s);
+    }
+
 }
